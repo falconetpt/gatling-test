e39e802451f5889310d985d3f6f0e8bff005bf1f
craigmacdonaldCC <craig.macdonald@currencycloud.com>
2021-07-30 15:23:20 +0100

    hamfisted attempt at stuff

--- src/test/scala/AkkaSimulation.scala
+++ src/test/scala/AkkaSimulation.scala
@@ -5,4 +5,14 @@ import scala.concurrent.duration._
 
 class AkkaSimulation extends Simulation {
 
+//  configurations here
+  val httpConfiguration = http.baseUrl("www.amazon.com")
+
+//  define the scenario
+  val scenarioName = scenario("amazon test")
+  .exec(
+    http("homepage test")
+      .get("").check(status is 200)
+  )
+
 }
