9dc51b354182d6c92959be612693e90229d4f8a2
Gareth Matthews <gareth.matthews@currencycloud.com>
2021-07-30 16:06:14 +0100

    added amqp (doesnt work)

--- src/test/scala/AkkaSimulation.scala
+++ src/test/scala/AkkaSimulation.scala
@@ -1,12 +1,23 @@
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+import ru.tinkoff.gatling.amqp.Predef._
+import ru.tinkoff.gatling.amqp.protocol.AmqpProtocolBuilder
 
 import scala.concurrent.duration._
 
 class AkkaSimulation extends Simulation {
-
 //  configurations here
-  val httpConfiguration = http.baseUrl("www.amazon.com")
+  val httpConfiguration = http.baseUrl("http://www.amazon.com")
+  
+  val amqpConfiguration = amqp
+  .connectionFactory(
+      rabbitmq
+        .host("localhost")
+        .port(5672)
+        .username("guest")
+        .password("guest")
+    )
+    .useNonPersistentDeliveryMode
 
 //  define the scenario
   val scenarioName = scenario("amazon test")
@@ -14,5 +25,12 @@ class AkkaSimulation extends Simulation {
     http("homepage test")
       .get("").check(status is 200)
   )
-
+    .exec(
+      http("book page")
+        .get("/music/unlimited?ref_=nav_em__dm_hf_0_2_2_2")
+        .check(status is 200)
+    )
+  setUp(
+    scenarioName.inject(atOnceUsers(3), constantUsersPerSec(10).during(10 seconds))
+  ).protocols(httpConfiguration)
 }
